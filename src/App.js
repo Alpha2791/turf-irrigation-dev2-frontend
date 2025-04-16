4a35aec957a41c4b81340489068136c907c674fa
Rob Hay <robhay@outlook.com>
2025-04-16 17:24:15 +0100

    Add welcome message and polish for tester

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ import IrrigationLog from './components/IrrigationLog';
 function App() {
   return (
     <div className="App">
-      <h1>Turf Tracker Soil Moisture Trail</h1>
+      <h1>Turf Tracker Soil Moisture Trial</h1>
+      <p style={{ fontSize: "1.1rem", marginBottom: "20px" }}>
+      Welcome to the Turf Tracker Soil Moisture Trial.<br />
+      Please log your irrigation applications and moisture readings. Your data will appear on the chart and feed into predictions.
+      </p>
+
       <MoistureForm />
       <hr />
       <IrrigationForm />
